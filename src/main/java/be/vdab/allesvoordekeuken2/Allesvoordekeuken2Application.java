032f4d077a8967535bfe102494392f4d3012e9ad
Arne <a.vaneycken@gmail.com>
2020-11-27 09:04:06 +0100

    initial commit

---
+++ src/main/java/be/vdab/allesvoordekeuken2/Allesvoordekeuken2Application.java
@@ -0,0 +1,13 @@
+package be.vdab.allesvoordekeuken2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Allesvoordekeuken2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Allesvoordekeuken2Application.class, args);
+	}
+
+}
---
+++ src/test/java/be/vdab/allesvoordekeuken2/Allesvoordekeuken2ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.allesvoordekeuken2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Allesvoordekeuken2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
